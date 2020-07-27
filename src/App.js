65f761ed06165f2e38c18a7f7c9958e1ba15548c
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-27 11:15:37 -0600

    Add updateItem() method. Add setState() example (newToDo).

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import React from 'react';
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
+class App extends React.Component
+{
+  constructor ( props )
+  {
+    super( props );
 
     this.state = {
-      newTodo: "",
-      toDos: []
+      newToDo: "", // Keep track of our new to-do value.
+      toDos: [] // Keep track of all the todos.
     };
   }
 
-
-  addToDo(event) {
-
-    event.preventDefault();
-    console.log("we are running");
-
+  addToDo ( event )
+  {
+    event.preventDefault(); // Stop the page from reloading.
+    console.log( "Test add todo!" );
   }
 
-  render() {
+  updateItem ( key, value )
+  {
+    // We never re-assign the contents of this.state.
+    // this.state is ONLY USED FOR READING VALUES, NOT writing.
+    // When we need to update anything in state, we need to use this.setState()
+    // this.setState() triggers the render() method, so we can see updated state info in our presentation.
+    this.setState( {[key]: value} );
+  }
+
+  render ()
+  {
     return (
       <>
         <h1>React To-Do App</h1>
-        <form onSubmit= {this.addToDo}>
-          <label htmlFor="newToDo">Enter a new task
-        <input type="text" name="addToDo" id="newToDo" required/>
+        <form onSubmit={this.addToDo}>
+          <label htmlFor="newToDo">
+            Enter a new "To-Do:"
+            <input
+              type="text"
+              name="newToDo"
+              id="newToDo"
+              required
+              value={this.state.newToDo}
+              onChange={event => this.updateItem( 'newToDo', event.target.value )} />
           </label>
-          <input type="submit" value="Add New Task" />
+          <input type="submit" value="Add New To-Do" />
         </form>
-
         <h2>Current To-Dos:</h2>
         <ul>
 
         </ul>
       </>
-    )
+    );
   }
 }
 
