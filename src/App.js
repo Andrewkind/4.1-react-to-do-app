09122eb0bdacc297d3ea5e513e5574bc55ea113b
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-27 10:28:16 -0600

    Add onSubmit event button. Prevent default form submission.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newTodo: "",
+      toDos: []
+    };
+  }
+
+
+  addToDo(event) {
+
+    event.preventDefault();
+    console.log("we are running");
+
+  }
+
   render() {
     return (
       <>
-      <h1>React To-Do App</h1>
-        <form> 
-      <label htmlFor="newToDo">Enter a new task
-        <input type="text" name="newToDo" id="newToDo" />
-      </label>
-      <input type="submit" value= "Add New Task" />
+        <h1>React To-Do App</h1>
+        <form onSubmit= {this.addToDo}>
+          <label htmlFor="newToDo">Enter a new task
+        <input type="text" name="addToDo" id="newToDo" required/>
+          </label>
+          <input type="submit" value="Add New Task" />
         </form>
-        
+
         <h2>Current To-Dos:</h2>
         <ul>
-          
+
         </ul>
       </>
     )
