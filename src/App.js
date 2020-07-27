8482ff3566a14362501fb2e7ae5aacfdda06a8d0
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-27 10:13:08 -0600

    Add New To-Do form. Add List.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello, World!</h1>
+      <>
+      <h1>React To-Do App</h1>
+        <form> 
+      <label htmlFor="newToDo">Enter a new task
+        <input type="text" name="newToDo" id="newToDo" />
+      </label>
+      <input type="submit" value= "Add New Task" />
+        </form>
+        
+        <h2>Current To-Dos:</h2>
+        <ul>
+          
+        </ul>
+      </>
     )
   }
 }
